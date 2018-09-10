ac604b15229cf963b68b049d1d91b227146bd6b7
Harry Gantz <harryhgantz@gmail.com>
2018-09-10 01:07:45 -0500

    sorted the titles alphabetically on shows page and on index I added the highest rated shows to the carousel

--- routes/shows.js
+++ routes/shows.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../dashboard.json');
+const dashboard = require('../dashboard.json');
+const _ = require('lodash');
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('shows', {
         title: 'HIDIVE:Shows',
-        dashboard: db
+        dashboard,
+        _,
+        titles: []
 
     });
+
 });
 
 
